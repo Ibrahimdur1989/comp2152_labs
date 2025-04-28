b11d6392b2fb4516d4310bc7b2b7fa68956ef03d
Ebrahim Al-Serri <Ibrahimdur1989@gmail.com>
2025-04-27 23:27:53 -0400

    Create lab1.py

---
+++ Week01 /lab1.py
@@ -0,0 +1,19 @@
+# Sample Coding Questions 01 Week 01
+# Ebrahim Al-Serri
+
+
+my_array = [1,4,7,9]
+
+a, b, c, d = 1, 2, 3, 4
+
+e = (a*c) - (b/d)
+
+e = ((a-(b**c))//d) + (a%c)
+
+temperature = 32.6
+
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter your age: "))
+
+print("The age is: {:} years old".format(userAge))
